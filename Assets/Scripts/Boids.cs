88c2a9075d9a3a0dc819b30733344cab03fe9f53
김현승 <corn120033@gmail.com>
2022-04-23 16:29:07 +0900

    Update Boids.cs
    BoidUnit의 속도를 조절하는 변수와 각 규칙의 가중치를 조절하는 변수를 추가한다

--- Assets/Scripts/Boids.cs
+++ Assets/Scripts/Boids.cs
@@ -6,25 +6,31 @@ public class Boids : MonoBehaviour
 {
     [SerializeField] private BoidUnit boidUnitPrefab;
     public int boidCount;
-    [SerializeField] private float spawnRange = 30;
+    public float spawnRange = 30;
+    [SerializeField] private Vector2 speedRange;
     //Cohesion - 주변 무리의 중심 방향으로 이동
     //Alignment - 주변 무리가 향하는 평균 방향으로 전환
     //Separation - 뭉쳐있는 무리를 피해 이동
+    public float cohesionWeight = 1;
+    public float alignmentWeight = 1;
+    public float separationWeight = 1;
+    public float boundsWeight = 1;
+
     void Start()
     {
         for (int i = 0; i < boidCount; i++)
         {
             Vector3 randomVec = Random.insideUnitSphere;
             randomVec *= spawnRange;
-
             Quaternion randomRot = Quaternion.Euler(0, Random.Range(0, 360f), 0);
             BoidUnit currUnit = Instantiate(boidUnitPrefab, this.transform.position + randomVec, randomRot);
             currUnit.transform.SetParent(this.transform);
+            currUnit.InitializeUnit(this, Random.Range(speedRange.x, speedRange.y));
         }
     }
 
     void Update()
     {
-        
+
     }
 }
